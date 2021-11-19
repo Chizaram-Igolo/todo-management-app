f86cec62b9a39f1988edaf9576bc8f9c085611a1
Chizaram-Igolo <chizaram@yahoo.com>
2021-11-19 14:33:38 +0100

    made some corrections in getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Form from "../components/form/Form";
 import styles from "../styles/Home.module.css";
 import { todoItem } from "../types/todoItem";
 
-import { connect } from "../utils/connection";
+import { MongoClient } from "mongodb";
 
 import ItemList from "./item-list";
 
@@ -46,41 +46,25 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   let items: todoItem[] = [];
 
-  const { client, todoitemsCollection } = await connect();
+  const client = await MongoClient.connect(
+    "mongodb+srv://chizaram:chizaram9@cluster0.wyjqa.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
 
-  if (client !== null && todoitemsCollection !== null) {
-    items = (await todoitemsCollection.find().toArray()) as todoItem[];
+  const todoitemsCollection = db.collection("todoitems");
 
-    client.close();
+  items = (await todoitemsCollection.find().toArray()) as todoItem[];
 
-    return {
-      props: {
-        items: items.map((item) => ({
-          content: item.content,
-          dueDate: item.dueDate,
-          status: item.status,
-          _id: item._id.toString(),
-        })),
-      },
-    };
-  } else {
-    console.log(`Couldn't connect to the database`);
-
-    // IF FAILURE OCCURS
-    let fallBackItem = [
-      {
-        content: "An Error occurred while trying to connect. Nothing to show",
-        dueDate: new Date(),
-        status: "",
-        _id: "1",
-      },
-    ];
-    return {
-      props: {
-        items: fallBackItem,
-      },
-    };
-  }
+  return {
+    props: {
+      items: items.map((item) => ({
+        content: item.content,
+        dueDate: item.dueDate,
+        status: item.status,
+        _id: item._id.toString(),
+      })),
+    },
+  };
 };
 
 export default Home;
